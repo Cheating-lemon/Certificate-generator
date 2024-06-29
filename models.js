3f9158ef05485127f32e6ff2b7a80d0ea24f9b9b
chaitanya <chaitanyaharshan.official@gmail.com>
2024-06-29 23:02:21 +0530

    commented few lines of not necessary code

--- models.js
+++ models.js
@@ -104,11 +104,11 @@ async function generateAndUploadPDFs() {
 
     for (const user of users) {
       const pdfStream = generatePDF(user);
-      console.log(typeof(pdfStream));
+      // console.log(typeof(pdfStream));
 
       // promise to handle stream events
       const uploadPromise = new Promise((resolve, reject) => {
-
+        
         pdfStream.on('finish', async () => {
           try {
             const result = await uploadToS3(pdfStream, user.roll_number);
